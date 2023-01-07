61878661acfe9a5899b4872f9abff17793e571be
Stéphane K <steffkarraz@hotmail.com>
2023-01-07 02:08:37 +0100

    day21 - part2

--- day21/day21.go
+++ day21/day21.go
@@ -2,38 +2,11 @@ package day21
 
 import (
 	"fmt"
+	"math"
 	"strings"
 
 	"github.com/Segmev/AdventOfCode2022/tools"
 )
-
-func inverseOpeLeftX(ope string) string {
-	switch ope {
-	case "+":
-		return "-"
-	case "-":
-		return "+"
-	case "*":
-		return "/"
-	case "/":
-		return "*"
-	}
-	panic("Operator not found")
-}
-
-func inverseOpeRightX(ope string) string {
-	switch ope {
-	case "+":
-		return "-"
-	case "-":
-		return "-"
-	case "*":
-		return "/"
-	case "/":
-		return "/"
-	}
-	panic("Operator not found")
-}
 
 func computeNbs(fnb, snb int, ope string) int {
 	switch ope {
@@ -99,38 +72,7 @@ func swapInstrs(instrNameA, instrNameB string, instrs map[string][]string) {
 	instrs[instrNameB] = tmp
 }
 
-func reverseHumnEquation(instrName string, instrs map[string][]string) {
-	fmt.Println(instrs["humn"])
-	fmt.Println(instrs[instrName])
-	if instrs[instrName][0] == "humn" {
-		instrs[instrName][1] = inverseOpeLeftX(instrs[instrName][1])
-		swapInstrs("humn", instrName, instrs)
-		instrs["humn"][0] = instrName
-	} else if instrs[instrName][2] == "humn" {
-		instrs[instrName][1] = inverseOpeRightX(instrs[instrName][1])
-		swapInstrs("humn", instrName, instrs)
-		instrs["humn"][2] = instrName
-
-	} else {
-		panic(fmt.Sprint("humn not found in", instrName, instrs[instrName]))
-	}
-	fmt.Println(instrs["humn"])
-	fmt.Println(instrs[instrName])
-}
-
-func reverseEquation(records []string, instrs map[string][]string) {
-	fmt.Println(instrs)
-
-	for i := 1; i < len(records); i++ {
-		fmt.Println("reversing", records[i-1], records[i])
-		reverseHumnEquation(records[i-1], instrs)
-		if instrs[records[i]][0] == records[i-1] {
-			instrs[records[i]][0] = "humn"
-		} else {
-			instrs[records[i]][2] = "humn"
-		}
-	}
-}
+const RATE = 0.01
 
 func partTwo(lines []string) {
 	numbers := map[string]int{}
@@ -143,7 +85,7 @@ func partTwo(lines []string) {
 		}
 
 		if len(tokens) == 1 {
-			numbers[instrParts[0]] = tools.Atoi(tokens[0])
+			numbers[instrParts[0]] = (tools.Atoi(tokens[0]))
 		} else {
 			instrs[instrParts[0]] = tokens
 			if tokens[0] == "humn" {
@@ -152,25 +94,38 @@ func partTwo(lines []string) {
 		}
 	}
 
-	fmt.Println(instrs)
-	delete(numbers, "humn")
+	numbers["humn"] = 0
+	previousGuessedNb := numbers["humn"]
 	calcTokens(instrs["root"][2], numbers, instrs)
 
-	records := &[]string{}
-	fmt.Println(hasHumnInstr("root", numbers, instrs, records))
-	fmt.Println(records)
-	reverseEquation(*records, instrs)
-	fmt.Println(instrs)
+	guessedNb := 0
+	errorDiff := math.Abs(float64(numbers[instrs["root"][2]]))
+	previousErrorDiff := 1.0
+	for errorDiff > 0.1 {
+		copiedNumbers := tools.CloneMap(numbers)
+		copiedNumbers["humn"] = guessedNb
+		calcTokens("root", copiedNumbers, instrs)
+		divider := int(errorDiff - previousErrorDiff)
+		var gradiant int
+		if divider == 0 {
+			if errorDiff < previousErrorDiff {
+				gradiant = 1
+			} else {
+				gradiant = -1
+			}
+		} else {
+			gradiant = (guessedNb - previousGuessedNb) / int(errorDiff-previousErrorDiff)
+		}
 
-	numbers[(*records)[0]] = numbers[instrs["root"][2]]
-
-	fmt.Println(numbers)
-	calcTokens("root", numbers, instrs)
-	fmt.Println(numbers)
-
-	fmt.Println(numbers["humn"])
-	fmt.Println(numbers[instrs["root"][0]])
-	fmt.Println(numbers[instrs["root"][2]])
+		previousGuessedNb = guessedNb
+		previousErrorDiff = errorDiff
+		guessedNb -= int((RATE * float64(gradiant) * errorDiff))
+		copiedNumbers = tools.CloneMap(numbers)
+		copiedNumbers["humn"] = guessedNb
+		calcTokens("root", copiedNumbers, instrs)
+		errorDiff = math.Abs(float64(copiedNumbers["root"]))
+	}
+	fmt.Println(guessedNb)
 }
 
 func Main(path string) {
